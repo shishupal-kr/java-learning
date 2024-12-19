85fd0257cab67813a490df271c56fee1c32542cf
Shishupal <shishupalking@gmail.com>
2024-12-19 21:36:35 +0530

    added code in arra

--- src/DSA/Array.java
+++ src/DSA/Array.java
@@ -1,11 +1,30 @@
 package DSA;
 
+import java.util.Arrays;
+
 public class Array {
-
-  int[] list = new int[10]
-  list = {1,2,3,4,5,6,7,8,9,10}
-
   public static void main(String[] args){
-    
+
+    int[] list = new int[10];
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+    list[3] = 4;
+    list[4] = 5;
+    list[5] = 6;
+    list[6] = 7;
+    list[7] = 8;
+    list[8] = 9;
+    list[9] = 10;
+    System.out.println(Arrays.toString(list));
+
+    //or
+    String[] fruit = {"apple","banana","orange","mango"};
+    System.out.println(Arrays.toString(fruit));
+
+
+    //best way to print is using loops
+    for(int i=0; i <= list.length; i++);
+    System.out.println(list);
   }
 }
