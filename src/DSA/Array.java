96c964fd2acc46123a53021947a829b67d1f2bfc
Shishupal <shishupalking@gmail.com>
2024-12-19 21:57:45 +0530

    complet code added in array

--- src/DSA/Array.java
+++ src/DSA/Array.java
@@ -5,17 +5,13 @@ import java.util.Arrays;
 public class Array {
   public static void main(String[] args){
 
-    int[] list = new int[10];
+    int[] list = new int[5];
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
     list[3] = 4;
     list[4] = 5;
-    list[5] = 6;
-    list[6] = 7;
-    list[7] = 8;
-    list[8] = 9;
-    list[9] = 10;
+
     System.out.println(Arrays.toString(list));
 
     //or
@@ -24,7 +20,13 @@ public class Array {
 
 
     //best way to print is using loops
-    for(int i=0; i <= list.length; i++);
-    System.out.println(list);
+    for(int i=0; i < list.length; i++){
+        System.out.println(list[i]);
+    }
+
+    for(int i=0; i < fruit.length; i++){
+      System.out.println(fruit[i]);
+    }
+
   }
 }
