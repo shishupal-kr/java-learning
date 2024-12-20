e66b3359355bf7f147f41f6c4f1970007470cd6a
Shishupal <shishupalking@gmail.com>
2024-12-20 20:16:03 +0530

    update code

--- src/Conditional_statement/nested_if.java
+++ src/Conditional_statement/nested_if.java
@@ -1,15 +1,22 @@
 package Conditional_statement;
+import java.util.Scanner;
 
+//In a nested if statement, an if statement is placed inside another if statement.
 public class nested_if {
     public static void main(String[] args) {
 
-        int age = 20;
-        int weight = 55;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.print("Enter your Weight: ");
+        int weight = sc.nextInt();
 
         if (age >= 18) {
             if (weight >= 50) {
                 System.out.println("You are eligible to donate blood.");
             }
+        }else{
+            System.out.println("you are not eligible to donate blood");
         }
 
     }
