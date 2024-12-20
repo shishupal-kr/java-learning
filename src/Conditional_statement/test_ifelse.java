903f95d3d7a9388c2605a438f677826e37c9acb7
Shishupal <shishupalking@gmail.com>
2024-12-20 20:17:55 +0530

    added codee

--- src/Conditional_statement/test_ifelse.java
+++ src/Conditional_statement/test_ifelse.java
@@ -1,6 +1,8 @@
+package Conditional_statement;
+
 import java.util.Scanner;
 
-public class conditional_statement {
+public class test_ifelse {
     public static void main(String[] args) {
 
      Scanner scanner = new Scanner(System.in);
