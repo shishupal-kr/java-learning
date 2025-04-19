985ef932926956513e369c5b6e2d1c04fb415ea0
Shishupal <shishupalking@gmail.com>
2025-04-19 19:37:54 +0530

    added is duplicate

--- src/DSA/Stack/parenthesis.java
+++ src/DSA/Stack/parenthesis.java
@@ -30,8 +30,40 @@ public class parenthesis {
         return stack.isEmpty();
     }
 
+    public static boolean isDuplicate(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == ')') {
+                int count = 0;
+
+                // Pop until an opening bracket is found
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    stack.pop();
+                    count++;
+                }
+
+                if (!stack.isEmpty()) {
+                    stack.pop(); // Pop the opening bracket
+                }
+
+                // If no characters between '()', it's a duplicate
+                if (count == 0) {
+                    return true;
+                }
+
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        String str  = "{([])}";
-        System.out.println(isValid(str));
+        String str  = "{[(]}}";
+        System.out.println(isDuplicate(str));
     }
 }
